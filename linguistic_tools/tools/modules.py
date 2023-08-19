96cdfaf35315450151504204b9ad82df891d072d
Elkrypton <rochdielmajdoub@gmail.com>
2023-08-19 17:02:23 -0400

    changing topic modeling

--- linguistic_tools/tools/modules.py
+++ linguistic_tools/tools/modules.py
@@ -5,8 +5,11 @@ try:
     import requests
     from bs4 import BeautifulSoup
     from gensim.corpora.dictionary import Dictionary
+    from gensim.models.ldamodel import LdaModel
     from typing import Optional
     import time
-    
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
 except ImportError as err:
     print(":::ERROR IMPORTING MODULE:: {}".format(str(err)))
--- linguistic_tools/tools/topic_model.py
+++ linguistic_tools/tools/topic_model.py
@@ -1,27 +1,16 @@
-try:
-    from nltk.stem import WordNetLemmatizer
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
-    import requests
-    from bs4 import BeautifulSoup
-    from gensim.corpora.dictionary import Dictionary
-    from typing import Optional
-    import time
-except ImportError as err:
-    print(":::ERROR IMPORTING MODULE:: {}".format(str(err)))
 
+from modules import *
 
 def fetch_article(url: str) -> str:
     """
     Fetches the article from the url and returns it as a string
     """
     response = requests.get(url)
-    soup = BeautifulSoup(response.content)
-    article = soup.find("div", class_ = "card-body-big")
-    full_text = article.get_text()
+    soup = BeautifulSoup(response.content, 'html.parser')
+    full_text = soup.get_text()
     return full_text
 
-def tokenize_article(article: str, extra_stops: Optional[list]: None ) -> list[str]:
+def tokenize_article(article: str, extra_stops: Optional[list] =  None ) -> list[str]:
     """
     Tokenizes the article into a list of words
     """
@@ -40,6 +29,69 @@ def tokenize_article(article: str, extra_stops: Optional[list]: None ) -> list[s
                 article_tokens.append(word)
     return article_tokens
 
+urls = [
+    'https://thecodinginterface.com/blog/spam-ham-ml-scikit-learn/',
+    'https://thecodinginterface.com/blog/opencv-Mat-from-array-and-vector/',
+    'https://thecodinginterface.com/blog/sql-window-functions/',
+    'https://thecodinginterface.com/blog/bridging-nodejs-and-python-with-pynode/',
+    'https://thecodinginterface.com/blog/django-auth-part1/',
+    'https://thecodinginterface.com/blog/django-auth-part4/',
+    'https://thecodinginterface.com/blog/intro-to-java-for-devs/',
+    'https://thecodinginterface.com/blog/text-analytics-app-with-flask-and-textblob/',
+    'https://thecodinginterface.com/blog/aws-s3-python-boto3/',
+    'https://thecodinginterface.com/blog/intro-to-pyspark/',
+    'https://thecodinginterface.com/blog/java-web-scraping-app-with-jsoup-and-javafx/',
+    'https://thecodinginterface.com/blog/javafx-alerts-and-dialogs/',
+    'https://thecodinginterface.com/blog/intro-to-pyflink/',
+    'https://thecodinginterface.com/blog/opencv-cpp-vscode/'
+]
+
+docs = []
+
+for url in urls:
+    article_txt = fetch_article(url)
+    time.sleep(2)
+    article_tokens = tokenize_article(article_txt, extra_stops=['div', 'java'])
+    docs.append(article_tokens)
+
+corpus_dict = Dictionary(docs)
 
 
 
+
+print("Count of terms before filteration : {}".format(len(corpus_dict)))
+
+
+corpus_dict.filter_extremes(no_below=1, no_above=0.5)
+
+print("Count of terms after filteration : {}".format(len(corpus_dict)))
+
+corpus = [corpus_dict.doc2bow(doc)for doc in docs]
+
+n_topics = 16
+lda = LdaModel(corpus, num_topics=n_topics, random_state=23, id2word=corpus_dict)
+topic_mat  = lda.get_topics()
+ids, cols = zip(*lda.id2word.items())
+df = pd.DataFrame(topic_mat, columns=cols)
+df.iloc[:, ::300]
+
+
+topics = lda.show_topics(num_topics=n_topics, num_words=5, formatted=False)
+topics = sorted(topics, key=lambda x: int(x[0]))
+
+rows = 4
+cols = 4
+
+fig, axs = plt.subplots(nrows=rows, ncols=cols, sharex=True, figsize=(12,8))
+
+for topic_id, word_props in topics:
+    row = topic_id // rows
+    col = topic_id - (row* cols)
+    ax  = axs[row, col]
+    words, probs= zip(*word_props)
+    ax.barh(words, probs)
+    ax.invert_yaxis()
+    ax.set_title('Topic {}'.format(topic_id))
+
+plt.tight_layout()
+plt.show()
